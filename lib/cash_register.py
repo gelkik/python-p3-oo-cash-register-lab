7490bcb6648369fb322848a011f12cb31b276a67
George Elkik <gelkik@gmail.com>
2023-02-24 11:45:05 -0500

    final

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+import ipdb;
 
 class CashRegister:
-  pass
+  def __init__(self,discount=0,total=0):
+    self.discount = discount
+    self.total = total
+    self.items = []
+    self.last_transaction = 0
+
+  def add_item(self,title='',price=0,quantity=1):
+    i = 0
+    self.last_transaction = price*quantity
+    while i < quantity:
+      self.items.append(title)
+      self.total += (price)
+      i+=1
+
+  def apply_discount(self):
+    self.total -= self.discount*10
+    if self.discount == 0:
+      print('There is no discount to apply.')
+    else:
+      print(f'After the discount, the total comes to ${self.total}.')
+
+  def void_last_transaction(self):
+    self.total -= self.last_transaction
